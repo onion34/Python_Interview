c43452f550bb560e688338cd51b3f05f578043af
klatmddud <klatmddud@gmail.com>
2022-08-25 16:09:21 +0900

    Add Reorder_Log_Files.py

---
+++ Reorder_Log_Files.py
@@ -0,0 +1,15 @@
+# Python Interview
+# Reorder Log Files
+
+def solution(logs: list[str]) -> list[str]:
+
+    letter_log = []
+    digit_log = []
+
+    for log in logs:
+
+        digit_log.append(log) if log.split()[1].isdigit() else letter_log.append(log)
+
+    letter_log.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+    
+    return letter_log + digit_log
